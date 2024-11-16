2d7f847c14d1a6591a2e0ef02f7568e9a840b195
angelycontrerasr <angelycontrerasr@gmail.com>
2024-11-15 21:41:23 -0400

    added add book button

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Happy developing ✨')
+const myLibrary = []
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return this.title + " by " + this.author + " has " + this.pages + " pages and is " + this.read;
+    };
+}
+
+function addBook(book) {
+    myLibrary.push(book);
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const dialog = document.getElementById("dialog");
+    const dialogBtn = document.getElementById("dialogBtn");
+    const closeBtn = document.getElementById("closeBtn");
+
+    // Open dialog when the "Add a Book" button is clicked
+    dialogBtn.addEventListener("click", () => {
+        dialog.showModal();
+    });
+
+    // Close dialog when the "Close" button is clicked
+    closeBtn.addEventListener("click", () => {
+        dialog.close();
+    });
+});
